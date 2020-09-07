9c5864edda580393cce81e0bf5b91eddd50ad40e
Daniel Park <ekklesia89@gmail.com>
2020-09-07 16:03:21 +0900

    about components color, main theme upgraded and svg icons added

--- src/common/theme.js
+++ src/common/theme.js
@@ -2,7 +2,9 @@ export const theme = {
   color: {
     background: "#f1f2f6",
     text: "#2f3542",
+    textInverse: "#f1f2f6",
     red: "#d72323",
     brown: "#3e3636",
+    main: "#2ed573",
   },
 };
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,17 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 import main from "../../assets/main.jpg";
+import programming from "../../assets/programming.png";
+import server from "../../assets/server.svg";
+import layout from "../../assets/layout.svg";
 
 const About = () => {
   return (
     <Container>
       <p>
-        <strong>Web/App Developer</strong>
-        <br />
-        <span>I code simple and modern things, and love what I do.</span>
+        <h3>Web/App Developer</h3>
+        <span>I code simple and modern things, and I love what I do.</span>
       </p>
       <div className="img-ring">
         <div className="cartoon-img" />
+      </div>
+      <div className="programming-img" />
+      <div className="description">
+        <p>
+          <h4>Hi, I'm Daniel. Glad to meet you.</h4>
+          <span>
+            Since beginning my journey as a developer, I've participated in
+            various service projects, and collaborated with talented people to
+            achieve great work perfomence. I'm naturally positive, reasonably
+            curious, and perpetually working on improving my skills and problems
+            at all time.
+          </span>
+        </p>
+      </div>
+      <div className="details">
+        <img src={server} alt="" className="server" />
+        <img src={layout} alt="" className="client" />
       </div>
     </Container>
   );
@@ -21,9 +40,9 @@ const About = () => {
 const Container = styled.div`
   color: ${(props) => props.theme.color.text};
   font-size: 2rem;
-  line-height: 1.5em;
   text-align: center;
   margin-top: 180px;
+  font-weight: 100;
 
   p {
     margin: 0;
@@ -31,6 +50,11 @@ const Container = styled.div`
 
   span {
     font-size: 1.2rem;
+  }
+
+  h3,
+  h4 {
+    margin: 0 0 16px;
   }
 
   .cartoon-img {
@@ -45,17 +69,55 @@ const Container = styled.div`
   }
 
   .img-ring {
-    border: 1px solid #fff;
+    border: 1px solid ${(props) => props.theme.color.main};
     width: 230px;
     height: 230px;
     border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-    margin: 36px auto;
+    margin: 36px auto 72px;
 
     &:hover {
-      border-color: #7bed9f;
+      box-shadow: 0px 0px 6px ${(props) => props.theme.color.main};
+    }
+  }
+
+  .programming-img {
+    background-image: url("${programming}");
+    width: 600px;
+    height: 300px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center left;
+    margin: 0 auto 8px;
+  }
+
+  .description {
+    width: 100%;
+    height: 320px;
+    background-color: ${(props) => props.theme.color.main};
+    padding: 84px 0;
+
+    p {
+      margin: 0 auto;
+      text-align: center;
+      width: 58%;
+      line-height: 2rem;
+      color: ${(props) => props.theme.color.textInverse};
+    }
+
+    span {
+      word-wrap: break-word;
+    }
+
+    .details {
+    }
+
+    .server,
+    .client {
+      width: 10%;
+      border: 1px solid red;
     }
   }
 `;
