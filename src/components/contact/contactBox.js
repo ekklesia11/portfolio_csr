24aeede715bac7530fa9ba40c901f889efc5ec49
Daniel Park <ekklesia89@gmail.com>
2020-09-08 20:29:03 +0900

    footer layout and link, icons updated

--- src/components/contact/contactBox.js
+++ src/components/contact/contactBox.js
@@ -1,32 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
-// import ContactWays from "./contactWays";
-// import ContactDirect from "./contactDirect";
-// import ContactSNS from "./contactSNS";
-
 import github from "../../assets/github-icon.png";
 import blog from "../../assets/blog-icon.png";
 import insta from "../../assets/insta-icon.png";
 import email from "../../assets/email-icon.png";
 
 const ContactBox = () => {
+  const contact = [
+    { name: "github", link: "https://github.com/ekklesia11", icon: github },
+    { name: "instagram", link: "https://instagram.com/d_paac", icon: insta },
+    { name: "blog", link: "https://blog.chanhyun.org", icon: blog },
+    { name: "email", link: "mailto:happyyquokka@gmail.com", icon: email },
+  ];
+
   return (
     <Container>
       <div className="word">It always seems impossible until it's done.</div>
-      <div>
-        <img src={github} alt="github" />
-        <img src={insta} alt="insta" />
-        <img src={blog} alt="blog" />
-        <img src={email} alt="email" />
+      <div className="info">
+        {contact.map((data) => (
+          <a
+            href={data.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={data.link}
+          >
+            <img src={data.icon} alt={data.name} />
+          </a>
+        ))}
       </div>
       <div className="copyright">Developed by my © 2020</div>
-      {/* <ContactWays>
-        <ContactDirect />
-      </ContactWays>
-      <ContactWays>
-        <ContactSNS />
-      </ContactWays> */}
     </Container>
   );
 };
@@ -34,7 +37,7 @@ const ContactBox = () => {
 const Container = styled.div`
   background-color: ${(props) => props.theme.color.main};
   color: ${(props) => props.theme.color.textInverse};
-  padding: 72px 0;
+  padding: 96px 0;
   text-align: center;
   font-weight: 200;
 
@@ -43,7 +46,29 @@ const Container = styled.div`
   }
 
   .copyright {
-    margin-top: 56px;
+    margin-top: 48px;
+  }
+
+  .info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a {
+      width: 56px;
+      height: 56px;
+      border: 1px solid ${(props) => props.theme.color.textInverse};
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 24px;
+    }
+
+    img {
+      display: block;
+      width: 24px;
+    }
   }
 `;
 
